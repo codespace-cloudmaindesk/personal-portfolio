a771531723a16be9510d3609ee05ba2df4944d81
tudinkojhb024 <tumisomphahle14@gmail.com>
2025-10-11 16:12:49 +0200

    intial commit

---
+++ src/frontend/src/types/index.ts
@@ -0,0 +1,12 @@
+export interface Project {
+    name: string;
+    description: string;
+    tech: string[];
+    link: string;
+}
+
+export interface ContactForm {
+    name: string;
+    email: string;
+    message: string;
+}
